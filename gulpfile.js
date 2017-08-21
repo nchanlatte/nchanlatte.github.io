8ee903272a89707f5c1296b0177c55c9e6cc6507
Matt Turco <matt_turco@gallup.com>
2017-08-21 15:37:29 -0500

    Simplify build script

--- gulpfile.js
+++ gulpfile.js
@@ -1,17 +1,8 @@
 // Common
 const gulp = require('gulp');
-const glob = require('glob');
+const runSequence = require('run-sequence');
 const del = require('del');
-const path = require('path');
-const merge = require('merge-stream');
-const source = require('vinyl-source-stream');
-const buffer = require('vinyl-buffer');
 const sourcemaps = require('gulp-sourcemaps');
-// JS
-const rollup = require('rollup-stream');
-const babel = require('rollup-plugin-babel');
-const babelrc = require('babelrc-rollup');
-const uglify = require('gulp-uglify');
 // CSS
 const sass = require('gulp-sass');
 const postcss = require('gulp-postcss');
@@ -24,24 +15,6 @@ const inline = require('gulp-inline-source');
 const BUILD = 'public';
 
 gulp.task('clean', () => del.sync([`${BUILD}/**`, `!${BUILD}`]));
-
-gulp.task('js', () => {
-  return merge(glob.sync('js/*.js').map(entry => {
-    return rollup({
-      entry,
-      sourceMap: true,
-      format: 'cjs',
-      plugins: [
-        babel(babelrc.default()),
-      ],
-    })
-    .pipe(source(path.resolve(entry), path.resolve('js')))
-    .pipe(buffer())
-    .pipe(sourcemaps.init({ loadMaps: true }))
-    .pipe(uglify({ preserveComments: 'license' }))
-    .pipe(sourcemaps.write('.'))
-  })).pipe(gulp.dest(`${BUILD}/js`));
-});
 
 gulp.task('css', () => {
   return gulp.src('sass/*.scss')
@@ -57,7 +30,7 @@ gulp.task('css', () => {
 
 gulp.task('copy-static', () => gulp.src('static/**').pipe(gulp.dest(BUILD)));
 
-gulp.task('html', ['js', 'css', 'copy-static'], () => {
+gulp.task('html', () => {
   return gulp.src('pages/**/*.html')
     .pipe(nunjucksRender({
       path: ['templates'],
@@ -69,11 +42,10 @@ gulp.task('html', ['js', 'css', 'copy-static'], () => {
 });
 
 
-gulp.task('default', ['clean', 'js', 'css', 'copy-static', 'html']);
+gulp.task('default', cb => runSequence('clean', ['css', 'copy-static'], 'html', cb));
 
 gulp.task('watch', ['default'], () => {
-  gulp.watch('js/**', ['js', 'html']);
-  gulp.watch('sass/**', ['css', 'html']);
-  gulp.watch('static/**', ['copy-static', 'html']);
+  gulp.watch('sass/**', cb => runSequence('css', 'html', cb));
+  gulp.watch('static/**', runSequence('copy-static', 'html', cb));
   gulp.watch(['templates/**', 'pages/**'], ['html']);
 });
